492a9c49d4f3edb454ed074a7e3fb8591e053c45
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-09-08 14:54:45 -0700

    WIP: server.js chat

--- chat/src/server.js
+++ chat/src/server.js
@@ -1,10 +1,24 @@
-var WebSocketServer = require('ws').Server
-  , wss = new WebSocketServer({port: 8080});
+var Bacon   = require('baconjs').Bacon;
+var express = require('express');
+var http    = require('http');
+var WSS     = require("ws").Server
 
-wss.on('connection', function(ws) {
-  ws.on('message', function(message) {
-    console.log('received: %s', message);
-  });
+var app     = express();
+var server  = http.createServer(app);
+var wss     = new WSS({ server: server, path: '/messages'});
 
-  ws.send('something');
+var connections = Bacon.fromEventTarget(wss, 'connection');
+var x = connections.flatMap(function(ws) {
+  return Bacon.fromEventTarget(ws, 'message');
 });
+
+var messages = (new Bacon.Bus()).combine(x)
+
+Bacon.zipAsArray(connections, messages).onValue(function(a) {
+  var ws = a[0], text = a[1].data;
+  ws.send(text);
+});
+
+server.listen(3003);
+
+console.log("Listening on port 3003");
