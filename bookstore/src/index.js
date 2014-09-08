626c263af0852f33554499c8bb63a1cc25eaa9ec
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-09-08 14:31:18 -0700

    removing functional lib, to simplify

--- bookstore/src/index.js
+++ bookstore/src/index.js
@@ -1,12 +1,9 @@
 var Bacon   = require('baconjs').Bacon;
 var express = require('express');
 
-var __    = require('./functional');
-var db    = require('./database');
+var db      = require('./database');
 
-var connections, response,                               // streams
-    fromRecordToResp, fromQueryToRecord, fromReqToQuery, // combinators
-    app = express();
+var connections, response, app = express();
 
 function streaminate(app, path) {
   var bus = new Bacon.Bus()
@@ -21,21 +18,15 @@ function streaminate(app, path) {
   return bus;
 }
 
-// return 'No match found' if null, val otherwise
-fromRecordToResp = __.iif(__.cnst('No match found'), __.id, __.neq(null));
-
-// given an http conn, returns its title param
-fromReqToQuery = __.compose(__.prop('title'), __.prop('params'), __.send('req'));
-
 // a stream of inbound HTTP connections
 connections = streaminate(app, '/books/:title');
 
 // a mapping of inbound HTTP connections to books in our
 // mongo database
 responses = connections
-  .map(fromReqToQuery)
+  .map(function(o) { return o.req().params.title; })
   .flatMap(Bacon.fromNodeCallback, db.getBookByTitle)
-  .map(fromRecordToResp)
+  .map(function(o) { return o === null ? 'No match found' : o; })
 
 // zip both streams together and respond
 Bacon.zipAsArray(connections, responses).onValues(function(conn, body) {
