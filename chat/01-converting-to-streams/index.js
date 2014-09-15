72e01e8d070637ace2306c94b8b0100a623c912d
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-09-15 13:00:02 -0700

    converting all callbacks to streams

--- chat/01-converting-to-streams/index.js
+++ chat/01-converting-to-streams/index.js
@@ -12,29 +12,42 @@ app.get('/', function(req, res){
 
 app.use(express.static(__dirname + '/static'));
 
-var connected = [];
+var connections = Bacon.fromBinder(function(sink) {
+  io.on('connection', sink)
+});
 
-var messages = new Bacon.Bus();
-
-io.on('connection', function(socket){
-  connected.push(socket);
-
-  socket.on('message', function(msg) {
-    messages.push(msg);
-  });
-
-  socket.on('disconnect', function() {
-    connected = _.filter(connected, function(s) {
-      return s.id != socket.id;
+var disconnects = connections.flatMap(function(socket) {
+  return Bacon.fromBinder(function(sink) {
+    socket.on('disconnect', function() {
+      sink(socket);
     });
   });
 });
 
-messages.onValue(function(msg) {
-  _.each(connected, function(s) {
-    s.send(msg);
-  });
+var connectDisconnects = connections
+  .map(function(socket) { return ['connection', socket]; })
+  .merge(disconnects.map(function(socket) { return ['disconnect', socket]; }));
+
+var active = connectDisconnects.scan([], function(acc, packed) {
+  var event = packed[0], socket = packed[1];
+  return event == 'connection'
+    ? acc.concat(socket)
+    : _.filter(acc, function(s) { return s.id != socket.id });
 });
+
+var messages = connections.flatMap(function(socket) {
+  return Bacon.fromEventTarget(socket, 'message');
+});
+
+active
+  .sampledBy(messages, function(sockets, message) {
+    return [sockets, message];
+  })
+  .onValues(function(sockets, message) {
+    _.each(sockets, function(s) {
+      s.send(message);
+    });
+  });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
