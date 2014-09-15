0dbcd62f13fa0a4ba95b90a71545705d701eb83b
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-09-15 13:54:02 -0700

    using broadcast and emit

--- chat/00-initial-setup/index.js
+++ chat/00-initial-setup/index.js
@@ -11,21 +11,15 @@ app.get('/', function(req, res){
 
 app.use(express.static(__dirname + '/static'));
 
-var connected = [];
+io.on('connection', function(socket) {
+  socket.broadcast.emit('message', 'User ' + socket.id + ' has connected.');
 
-io.on('connection', function(socket){
-  connected.push(socket);
-
-  socket.on('message', function(msg){
-    _.each(connected, function(s) {
-      s.send(msg);
-    });
+  socket.on('message', function(msg) {
+    io.emit('message', '' + socket.id + ': ' + msg);
   });
 
   socket.on('disconnect', function() {
-    connected = _.filter(connected, function(s) {
-      return s.id != socket.id;
-    });
+    io.sockets.emit('User ' + socket.id + ' has disconnected.');
   });
 });
 
