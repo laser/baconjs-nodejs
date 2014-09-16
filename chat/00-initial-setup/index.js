b4664996c18e7a757fc12ac77b22bcded0577ca0
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-09-16 08:25:57 -0700

    adding fs read/write to example

--- chat/00-initial-setup/index.js
+++ chat/00-initial-setup/index.js
@@ -1,28 +1,52 @@
-var _       = require('underscore')
-var path    = require('path')
-var express = require('express')
-var app     = express();
-var http    = require('http').Server(app);
-var io      = require('socket.io')(http);
+var _       = require('underscore'),
+    express = require('express'),
+    fs      = require('fs'),
+    app     = express(),
+    http    = require('http').Server(app),
+    io      = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'static', 'index.html'));
+app.get('/', function(req, res) {
+  res.sendFile('./static/index.html');
 });
 
-app.use(express.static(__dirname + '/static'));
+app.use(express.static('./static'));
 
-io.on('connection', function(socket) {
-  socket.broadcast.emit('message', 'User ' + socket.id + ' has connected.');
+function log(level, message) {
+  var logger = level == 'debug' ? 'log' : 'error';
+  console[logger](message);
+}
 
-  socket.on('message', function(msg) {
-    io.emit('message', '' + socket.id + ': ' + msg);
-  });
+fs.readFile('./hello.txt', 'utf8', function(err, greeting) {
+  if (err) {
+    log('error', err);
+    throw err
+  }
 
-  socket.on('disconnect', function() {
-    io.emit('message', 'User ' + socket.id + ' has disconnected.');
+  io.on('connection', function(socket) {
+
+    socket.broadcast.emit('message', 'CONN: ' + socket.id);
+
+    socket.send(greeting);
+
+    socket.on('message', function(msg) {
+      var path = './logs/' + socket.id + '.log';
+
+      fs.appendFile(path, msg + '\n', function(err) {
+        if (err) {
+          log('error', err);
+          throw err
+        }
+      });
+
+      io.emit('message', socket.id + ': ' + msg);
+    });
+
+    socket.on('disconnect', function() {
+      io.emit('message', 'DISC: ' + socket.id);
+    });
   });
 });
 
-http.listen(3000, function(){
+http.listen(3000, function() {
   console.log('listening on *:3000');
 });
